bc278bd92b36e11bc3ceccf00a917cb92ac14399
Julien Pivotto <roidelapluie@inuits.eu>
2017-03-21 21:53:23 +0100

    Fix upload remote
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- upload.sh
+++ upload.sh
@@ -4,4 +4,4 @@ eval "$(ssh-agent -s)"
 chmod 600 deploy_key
 ssh-add deploy_key
 git commit -m 'Compiled!' dist
-git push origin HEAD:compiled
+git push git@github.com:roidelapluie/bootstrap.git HEAD:compiled
