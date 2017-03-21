f28c3848817a562827868404315c3ffd3f759682
Julien Pivotto <roidelapluie@inuits.eu>
2017-03-21 21:55:52 +0100

    Deploy only the v4-dev branch
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- upload.sh
+++ upload.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [[  -n $TRAVIS_BRANCH ]] && [[ $TRAVIS_BRANCH == "v4-dev" ]]
+    then
+
 eval "$(ssh-agent -s)"
 chmod 600 deploy_key
 ssh-add deploy_key
 git commit -m 'Compiled!' dist
 git push git@github.com:roidelapluie/bootstrap.git HEAD:compiled
+else
+    echo not pushing
+fi
